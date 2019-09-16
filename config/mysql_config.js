938cd91fba3ea247b915c3f9563464005ca1fa72
zhanglei25 <zhanglei25@le.com>
2019-09-16 15:59:19 +0800

    [chore](mysql_config.js)change mysql password

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,16 +2,15 @@
  * @Author: zhanglei
  * @Date: 2019-07-15 16:09:53
  * @LastEditors: zhanglei
- * @LastEditTime: 2019-09-02 19:40:01
+ * @LastEditTime: 2019-09-16 15:44:13
  * @Description:
  */
 const mysqlConfig = {
-    user: "root",
-    password: '!@#5Demysql',
-    database: 'newblog',
+    user: 'root',
+    password: '@5Demysql',
+    database: 'blog',
     host: '152.136.121.90',
     port: '3306'
-}
+};
 
 module.exports = mysqlConfig;
-
