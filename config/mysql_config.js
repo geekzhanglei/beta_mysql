4acbee8fcc28affa7ed1b6dd92d504a636ce5035
zhangleibj <zhangleibj@fenbi.com>
2021-05-20 23:30:19 +0800

    [chore](mysql_config.js) update mysql host

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,14 +2,14 @@
  * @Author: zhanglei
  * @Date: 2019-07-15 16:09:53
  * @LastEditors: zhanglei
- * @LastEditTime: 2019-09-16 15:44:13
+ * @LastEditTime: 2021-05-20 23:12:00
  * @Description:
  */
 const mysqlConfig = {
     user: 'root',
     password: '@5Demysql',
     database: 'blog',
-    host: '152.136.121.90',
+    host: '49.232.13.144',
     port: '3306'
 };
 
