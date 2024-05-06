a384166b60d0fa6b90d4cf30122066fdcd5a139d
zhanglei <leizhang219495@sohu-inc.com>
2024-05-06 14:53:41 +0800

    chore: 更新数据库密码

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,14 +2,14 @@
  * @Author: zhanglei
  * @Date: 2019-07-15 16:09:53
  * @LastEditors: zhanglei
- * @LastEditTime: 2021-05-20 23:12:00
+ * @LastEditTime: 2024-05-06 14:53:15
  * @Description:
  */
 const mysqlConfig = {
     user: 'root',
     password: '@5Demysql',
     database: 'blog',
-    host: '49.232.13.144',
+    host: '211.159.169.12',
     port: '3306'
 };
 
