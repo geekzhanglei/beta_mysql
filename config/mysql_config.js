709b68cfb8768b7a5983f87a4e92b44353bffc93
zhanglei <leizhang219495@sohu-inc.com>
2026-03-23 17:48:12 +0800

    chore: 自动cicd

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,15 +2,15 @@
  * @Author: zhanglei
  * @Date: 2019-07-15 16:09:53
  * @LastEditors: zhanglei
- * @LastEditTime: 2024-05-06 14:53:15
+ * @LastEditTime: 2026-03-23 17:08:00
  * @Description:
  */
 const mysqlConfig = {
-    user: 'root',
-    password: '@5Demysql',
-    database: 'blog',
-    host: '211.159.169.12',
-    port: '3306'
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'blog',
+    host: process.env.DB_HOST || '211.159.169.12',
+    port: process.env.DB_PORT || '3306'
 };
 
 module.exports = mysqlConfig;
